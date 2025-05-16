03a373663c704a47ecae0cb24e5ca76222fe2c54
Raffaella <45825990+raffaellasuardini@users.noreply.github.com>
2025-05-16 19:13:52 +0200

    feat: added next argument to volunteer and unvolunteer commands

--- cogs.py
+++ cogs.py
@@ -4,6 +4,7 @@ import json
 import aiofiles
 import aiosqlite
 import arrow
+import logging
 from discord.ext import commands
 
 
@@ -25,13 +26,13 @@ class VolunteerCog(commands.Cog):
         current_date = arrow.utcnow().format("YYYY-MM-DD")
 
         async with conn.execute(
-            """
+                """
             SELECT due_date
             FROM volunteers
             WHERE due_date > ? AND is_taken = 0
             LIMIT 10
             """,
-            (current_date,),
+                (current_date,),
         ) as cursor:
             rows = await cursor.fetchall()
 
@@ -42,6 +43,29 @@ class VolunteerCog(commands.Cog):
             f"- {arrow.get(row[0]).format('Do MMMM YYYY')}" for row in rows
         )
 
+    @staticmethod
+    async def _get_next_available_date(conn: aiosqlite.Connection) -> str | None:
+        """ Returns the next available date (the closer with is_taken = 0). """
+
+        current_date = arrow.utcnow().format("YYYY-MM-DD")
+
+        async with conn.execute(
+                """
+            SELECT due_date
+            FROM volunteers
+            WHERE due_date > ? AND is_taken = 0
+            ORDER BY due_date ASC
+            LIMIT 1
+            """,
+                (current_date,),
+        ) as cursor:
+            row = await cursor.fetchone()
+            if row:
+                logging.info(f"Next available date found : {row[0]}")
+            else:
+                logging.info(f"No available date.")
+            return row[0] if row else None
+
     @commands.command(name="available")
     async def available(self, ctx):
         response = await VolunteerCog._list_available_dates(self.cursor)
@@ -49,7 +73,7 @@ class VolunteerCog(commands.Cog):
 
     @staticmethod
     async def _update_volunteer_status(
-        conn: aiosqlite.Connection, date: str, name: str, is_taken: int
+            conn: aiosqlite.Connection, date: str, name: str, is_taken: int
     ) -> bool:
         query = """
             UPDATE volunteers
@@ -59,70 +83,122 @@ class VolunteerCog(commands.Cog):
             WHERE
                 due_date = ? AND (? = 1 OR name = ?)"""
         async with conn.execute(
-            query, (is_taken, is_taken, name, date, is_taken, name)
+                query, (is_taken, is_taken, name, date, is_taken, name)
         ) as cursor:
             await conn.commit()
             return cursor.rowcount > 0
 
     async def _handle_volunteer_action(
-        self,
-        ctx,
-        action: str,  # "assign" or "unassign"
-        success_msg: str,
-        failure_msg: str,
-        post_success_note: str = "",
+            self,
+            ctx,
+            action: str,  # "assign" or "unassign"
+            success_msg: str,
+            failure_msg: str,
+            date: str = None,
+            post_success_note: str = "",
     ):
-        await ctx.send("Please provide a date in the format YYYY-MM-DD.")
-        try:
-            response = await self.bot.wait_for(
-                "message", check=VolunteerCog._is_date_correct, timeout=60
-            )
-            date = arrow.get(response.content.strip(), "YYYY-MM-DD").format(
-                "YYYY-MM-DD"
-            )
-            is_taken = 1 if action == "assign" else 0
+        if not date:
+            await ctx.send("Please provide a date in the format YYYY-MM-DD.")
+            try:
+                response = await self.bot.wait_for(
+                    "message", check=VolunteerCog._is_date_correct, timeout=60
+                )
+                date = arrow.get(response.content.strip(), "YYYY-MM-DD").format(
+                    "YYYY-MM-DD"
+                )
 
-            updated = await VolunteerCog._update_volunteer_status(
-                self.cursor, date, ctx.author.display_name , is_taken
-            )
+            except asyncio.TimeoutError:
+                await ctx.send("You took too long. Please try again.")
 
-            if updated:
-                await ctx.send(success_msg.format(date=date))
-                if post_success_note:
-                    await ctx.send(post_success_note)
-            else:
-                await ctx.send(failure_msg)
-        except asyncio.TimeoutError:
-            await ctx.send("You took too long. Please try again.")
+        is_taken = 1 if action == "assign" else 0
+
+        updated = await VolunteerCog._update_volunteer_status(
+            self.cursor, date, ctx.author.display_name, is_taken
+        )
+
+        if updated:
+            await ctx.send(success_msg.format(date=date))
+            if post_success_note:
+                await ctx.send(post_success_note)
+        else:
+            await ctx.send(failure_msg)
 
     @commands.command(name="volunteer")
-    async def volunteer(self, ctx):
-        await self._handle_volunteer_action(
-            ctx,
-            action="assign",
-            success_msg="You have been assigned to {date}.",
-            failure_msg="Could not assign you. Try again.",
-        )
+    async def volunteer(self, ctx, option: str = None):
+        logging.info(f"Command !volunteer received from {ctx.author.display_name}, option={option}")
+        if option and option.lower() == "next":
+            next_date = await VolunteerCog._get_next_available_date(self.cursor)
+            if not next_date:
+                await ctx.send("No available dates found.")
+                return
+            await self._handle_volunteer_action(
+                ctx,
+                action="assign",
+                success_msg="You have been assigned to {date}.",
+                failure_msg="Could not assign you to {date}. Try again.",
+                date=next_date,
+            )
+        else:
+            await self._handle_volunteer_action(
+                ctx,
+                action="assign",
+                success_msg="You have been assigned to {date}.",
+                failure_msg="Could not assign you. Try again.",
+            )
+
+    @staticmethod
+    async def get_user_first_assigned_date(conn: aiosqlite.Connection, ctx):
+        """ Return the next assigned date to the user. """
+        async with conn.execute(
+            """
+            SELECT due_date
+            FROM volunteers
+            WHERE name = ? and is_taken = 1
+            ORDER BY due_date ASC
+            LIMIT 1
+            """,
+                (ctx.author.display_name,),
+        ) as cursor:
+            row = await cursor.fetchone()
+
+        if not row:
+            await ctx.send("You have no assigned date.")
+
+        return row[0] if row else None
 
     @commands.command(name="unvolunteer")
-    async def unvolunteer(self, ctx):
-        await self._handle_volunteer_action(
-            ctx,
-            action="unassign",
-            success_msg="You have been unassigned from {date}.",
-            failure_msg="Could not unassign you. Try again.",
-            post_success_note="Please inform folks on django-news channel so others can pick it up.",
-        )
+    async def unvolunteer(self, ctx, option: str = None):
+        if option and option.lower() == "next":
+            next_date = await VolunteerCog.get_user_first_assigned_date(self.cursor, ctx)
+            if not next_date:
+                await ctx.send("You don't have a shift.")
+            await self._handle_volunteer_action(
+                ctx,
+                action="unassign",
+                success_msg="You have been unassigned to {date}.",
+                failure_msg="Could not unassign you to {date}. Try again.",
+                post_success_note="Please inform folks on django-news channel so others can pick it up.",
+                date=next_date
+            )
+
+        else:
+            await self._handle_volunteer_action(
+                ctx,
+                action="unassign",
+                success_msg="You have been unassigned from {date}.",
+                failure_msg="Could not unassign you. Try again.",
+                post_success_note="Please inform folks on django-news channel so others can pick it up.",
+            )
 
     @commands.command(name="mydates")
     async def get_user_assigned_dates(self, ctx):
         async with self.cursor.execute(
-            """
+                """
             SELECT due_date, status
             FROM volunteers
             WHERE name = ?
             """,
-            (ctx.author.display_name,),
+                (ctx.author.display_name,),
         ) as cursor:
             rows = await cursor.fetchall()
 
@@ -142,12 +218,12 @@ class VolunteerCog(commands.Cog):
     async def get_date_status(self, ctx):
         current_date = arrow.utcnow().format("YYYY-MM-DD")
         async with self.cursor.execute(
-            """
+                """
             SELECT due_date, status, name
             FROM volunteers
             WHERE due_date >= ? AND is_taken = 1
             """,
-            (current_date,),
+                (current_date,),
         ) as cursor:
             rows = await cursor.fetchall()
         if not rows:
