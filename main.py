d99c7a301f84caf3abe179002761e5e5c88baf57
Raffaella <45825990+raffaellasuardini@users.noreply.github.com>
2025-07-28 17:05:58 +0200

    chore: removed double import logging

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from dotenv import load_dotenv
 src_path = Path(__file__).parent / "src"
 sys.path.insert(0, str(src_path))
 
-import logging
 from logging.handlers import RotatingFileHandler
 
 import structlog
